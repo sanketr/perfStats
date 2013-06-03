830f479c915ff14505a3e81c3745bafa3d573ee2
Sanket <sanket.agrawal@gmail.com>
2013-06-03 19:45:47 -0400

    Documentation

--- lcs/haskell/Miller.hs
+++ lcs/haskell/Miller.hs
@@ -23,6 +23,7 @@ import Control.Monad.Primitive (PrimState)
 import Control.Monad (when) 
 import GHC.Float.RealFracMethods (int2Float)
 import Data.STRef (newSTRef, modifySTRef, readSTRef)
+import Data.Word
 import Criterion.Main
 import Criterion.Config
 
@@ -41,7 +42,7 @@ cmp a b i j = go a b 0 i j
                     m = U.length b
 
 newVI1 :: Int -> Int -> ST s (MVI1 s)
-newVI1 n !x = do 
+newVI1 n x = do 
           a <- new n
           mapM_ (\i -> MU.unsafeWrite a i x) [0..n-1]
           return a 
@@ -56,6 +57,9 @@ sizesnakev :: Snakev s -> Int
 sizesnakev (S i _) = i
 {-#INLINE sizesnakev #-}
 
+-- Growable array - we always append an element. It grows by factor of 1.5 if more capacity is needed
+-- See FB note below about why a growth factor lower than 2 is better
+-- https://github.com/facebook/folly/blob/master/folly/docs/FBVector.md
 append :: Snakev s -> (Int,Int,Int,Int) -> ST s (Snakev s)
 append (S i v) !x = do
    if i < MU.length v then MU.unsafeWrite v i x >> return (S (i+1) v)
@@ -87,11 +91,16 @@ gridWalk a b fp snodes snakesv !k cmp = do
    return snakesv
 {-#INLINE gridWalk #-}
 
+-- As noted in the paper, we find furthest point given diagonal k, and current set of furthest points. 
+-- The function below executes ct times, and updates furthest point (fp), snake node indices in snake
+-- array (snodes), and inserts new snakes in snake array as they are found during furthest point search
 findSnakes :: (U.Unbox a, Eq a) => Vector a -> Vector a -> MVI1 s -> MVI1 s -> Snakev s -> Int -> Int -> (Vector a -> Vector a -> Int -> Int -> Int) -> (Int -> Int -> Int) -> ST s (Snakev s)
 findSnakes a b fp snodes snakesv !k !ct cmp op = do
   U.foldM (\s x -> gridWalk a b fp snodes s (op k x) cmp) snakesv (U.fromList [0..ct-1])
 {-#INLINE findSnakes #-}
 
+-- while tail-recursive loop courtesy of Gabriel Gonzalez
+-- see http://www.haskellforall.com/2012/01/haskell-for-c-programmers-for-loops.html
 while :: (Monad m) => m Bool -> m a -> m ()
 while !cond !action = do
       c <- cond
@@ -99,9 +108,15 @@ while !cond !action = do
         action
         while cond action
 
+-- Fills array v at index [i..i+l-1] with values [x..x+l-1]
 fill :: MVI1 s -> Int -> Int -> Int -> ST s ()
 fill v i x l = U.forM_ (U.fromList [0..l-1]) (\idx -> MU.unsafeWrite v (i+idx) (x+idx))
 
+-- iteration through snake array - we start at last snake in the array which is the snake
+-- ending at destination (n,m) when p was found. First element in each entry stores location 
+-- of previous snake node in snake array. We set index of next snake to that first element, 
+-- and continue iterating backwards until we get to origin (0,0). At origin, the index of
+--  previous node is -1. This is used as loop termination condition.
 iter :: Snakev s -> MVI1 s -> MVI1 s -> ST s ()
 iter (S len v) a b = do
    il <- newSTRef (len-1)
@@ -122,30 +137,33 @@ lcsh a b flip = runST $ do
       m = U.length b
       delta = m-n
       offset=n+1
-  fp <- newVI1 (m+n+3) (-1)
-  snodes <- newVI1 (m+n+3) (-1)
-  snakesv <- newSnakes (m+n+1)
-  p <- newSTRef 0
-  s <- newSTRef snakesv
+  fp <- newVI1 (m+n+3) (-1) -- array of furthest points
+  snodes <- newVI1 (m+n+3) (-1) -- array to keep track of snake nodes in each iteration
+  snakesv <- newSnakes (m+n+1) -- array of snakes - how does that sound?
+  p <- newSTRef 0 -- minimum number of deletions for a
+  s <- newSTRef snakesv -- store reference to snakevector - we have to pass it around
   while (MU.unsafeRead fp (delta+offset) >>= \x -> return $! (x<m))
     $ do 
       n <- readSTRef p
       s0 <- readSTRef s
-      s1 <- findSnakes a b fp snodes s0 (-n) (delta+n) cmp (+)
-      s2 <- findSnakes a b fp snodes s1 (delta+n) n cmp (-)
-      s3 <- findSnakes a b fp snodes s2 delta 1 cmp (-)
-      modifySTRef s (\_ -> s3)
-      modifySTRef p (+1)
-  lcslen <- (readSTRef p >>= \x -> return $ (U.length a)-(x-1))
+      s1 <- findSnakes a b fp snodes s0 (-n) (delta+n) cmp (+) -- vertical traversal
+      s2 <- findSnakes a b fp snodes s1 (delta+n) n cmp (-) -- horizontal traversal
+      s3 <- findSnakes a b fp snodes s2 delta 1 cmp (-) -- diagonal traversal
+      modifySTRef s (\_ -> s3) -- store the latest snakevec for next iteration
+      modifySTRef p (+1) -- increment p by 1
+  -- length of LCS is n-p. p must be decremented by 1 first for correct value of p 
+  lcslen <- (readSTRef p >>= \x -> return $ (U.length a)-(x-1)) 
   snakesv <- readSTRef s
-  a1 <- MU.new lcslen
+  a1 <- MU.new lcslen -- array to store LCS indices for a and b respectively
   b1 <- MU.new lcslen
-  iter snakesv a1 b1
+  iter snakesv a1 b1 -- iterate through snake vector, and fill up a1,b1 with indices of match
   a1 <- U.unsafeFreeze a1
   b1 <- U.unsafeFreeze b1
-  if flip then return (b1,a1)
+  if flip then return (b1,a1) -- maintain correct order when returning indices
   else return (a1,b1)
 
+-- Function to find longest common subsequence given unboxed vectors a and b
+-- It returns indices of LCS in a and b
 lcs :: (U.Unbox a, Eq a) => Vector a -> Vector a -> (Vector Int,Vector Int)
 lcs a b | (U.length a > U.length b) = lcsh b a True
         | otherwise = lcsh a b False
@@ -161,6 +179,5 @@ suite = [
           bench "lcs 10" $ whnf (lcs a) b
         ]
 
---main = print $ lcs (U.fromList ['a'..'e'])  (U.fromList ['b'..'c'])
 main :: IO()
 main = defaultMainWith config (return ()) suite
